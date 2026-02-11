aa9362a9333475c98189b0a93071350262ebdcbd
Mithali <mithali.salian@codecraft.co.in>
2026-02-11 14:55:45 +0530

    initual commit

---
+++ math.ts
@@ -0,0 +1,56 @@
+/**
+ * Add 2 numbers
+ * @param a 
+ * @param b 
+ * @returns 
+ */
+function add(a: number, b: number): number {
+    return a + b;
+}
+
+
+/**
+ * 
+ * @param a subtracts a number by another
+ * @param b 
+ * @returns 
+ */
+function subtract(a: number, b: number): number {
+    return a - b;
+}
+
+function multiply(a: number, b: number): number {
+    return a * b;
+
+}
+
+function divide(a: number, b: number): number {
+    return a / b;
+
+}
+
+function square(a: number, b: number): number {
+    return a * a;
+}
+function cube(a: number, b: number): number {
+    return a * a * a;
+
+}
+function cuberoot(a: number, b: number): number {
+    return Math.cbrt(a);
+}
+function sqroot(a: number, b: number): number {
+    return Math.sqrt(a);
+
+}
+
+function floor(b: number): number {
+    return Math.floor(b);
+}
+function divide1(a: number, b: number): number {
+    if (b == 0) {
+        throw new Error("divide by zero");
+    }
+    return a / b;
+
+}

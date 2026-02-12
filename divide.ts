9a27d9f9531a8d469c0921488a7c36704b6d7dd3
Mithali <mithali.salian@codecraft.co.in>
2026-02-12 12:23:57 +0530

    implemented divide function

--- divide.ts
+++ divide.ts
@@ -1,3 +1,12 @@
+/**
+ * divide a by b
+ * @param a 
+ * @param b 
+ * @returns 
+ */
 function divide(a: number, b: number): number {
+    if (b === 0) {
+        throw new Error("divide by zero");
+    }
     return a / b;
 }

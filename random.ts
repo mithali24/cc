a0da7700d5ae20328dd415d63cd93b4c4b25122b
Mithali <mithali.salian@codecraft.co.in>
2026-02-12 12:20:37 +0530

    implemented divide

--- random.ts
+++ random.ts
@@ -5,3 +5,10 @@ function randomNumber(a: number) {
 function randomno2(a: number): number {
     return Math.random();
 }
+function randomNumberadded(a: number) {
+    return Math.random() * a ** 2;
+}
+
+function random(a: number, b: number): number {
+    return Math.random() * (b - a) + a;
+}
